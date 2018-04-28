9ab08789358961808b29d9a4e287d71984c3fe31
teddyLuu <191695499@qq.com>
2018-04-28 19:38:54 +0800

    rate limit

--- routes/api.php
+++ routes/api.php
@@ -18,11 +18,20 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api'
 ], function ($api) {
-    //短信验证码
-    $api->post('verificationCodes', 'VerificationCodesController@store')
-        ->name('api.verificationCodes.store');
 
-    //用户注册
-    $api->post('users', 'UsersController@store')
-        ->name('api.users.store');
+    //使用Dingo提供的调用频率限制中间件，暂定时间为1分钟1次
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+    ], function ($api) {
+        //短信验证码
+        $api->post('verificationCodes', 'VerificationCodesController@store')
+            ->name('api.verificationCodes.store');
+
+        //用户注册
+        $api->post('users', 'UsersController@store')
+            ->name('api.users.store');
+    });
+
 });
