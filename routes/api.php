20ef4f7caf8b3c255344866207039938a682fc19
teddyLuu <191695499@qq.com>
2018-05-02 14:15:58 +0800

    add jwt

--- routes/api.php
+++ routes/api.php
@@ -42,6 +42,18 @@ $api->version('v1', [
         $api->post('socials/{social_type}/authorization', 'AuthorizationsController@socialStore')
             ->name('api.socials.authorizations.store');
 
+        //登录
+        $api->post('authorizations', 'AuthorizationsController@store')
+            ->name('api.socials.authorizations.store');
+
+        //刷新token
+        $api->put('authorizations/current', 'AuthorizationsController@update')
+            ->name('api.authorizations.update');
+
+        //删除token
+        $api->delete('authorizations/current', 'AuthorizationsController@destroy')
+            ->name('api.authorizations.destroy');
+
         //需要token验证的接口
         $api->group(['middleware' => 'api.auth'], function ($api) {
             //当前登录用户信息
