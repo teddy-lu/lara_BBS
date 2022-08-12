d2676d6be0e2d54be8608f407c4a250a45781e9f
teddyLuu <teddy@ifreegroup.com>
2022-08-12 18:16:21 +0800

    update: update laravel-framework version

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -14,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-		\App\Models\User::observe(\App\Observers\UserObserver::class);
+        \App\Models\User::observe(\App\Observers\UserObserver::class);
         \App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
         \App\Models\Topic::observe(\App\Observers\TopicObserver::class);
         \App\Models\Link::observe(\App\Observers\LinkObserver::class);
@@ -22,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
         // Carbon 中文化配置
         \Carbon\Carbon::setLocale('zh');
 
+        //防止重复编码的行为
+        Blade::withoutDoubleEncoding();
+
+        Paginator::useBootstrapThree();
     }
 
     /**
@@ -31,16 +37,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-    	if (app()->isLocal()) {
-    		$this->app->register(\VIACreative\SudoSu\ServiceProvider::class);
-    	}
+        if (app()->isLocal()) {
+            $this->app->register(\VIACreative\SudoSu\ServiceProvider::class);
+        }
 
-    	\API::error(function (\Illuminate\Database\Eloquent\ModelNotFoundException $exception){
-    	    abort(404);
+        \API::error(function (\Illuminate\Database\Eloquent\ModelNotFoundException $exception) {
+            abort(404);
         });
 
-    	\API::error(function (\Illuminate\Auth\Access\AuthorizationException $exception){
-    	    abort(403, $exception->getMessage());
+        \API::error(function (\Illuminate\Auth\Access\AuthorizationException $exception) {
+            abort(403, $exception->getMessage());
         });
     }
 }
